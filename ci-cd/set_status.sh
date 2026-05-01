65ab8d0144fb04fe6749e598300b62a1058d2bc8
ogiusek <kowalewski.olgierd@gmail.com>
2026-05-01 13:59:06 +0200

    modified Jenkinsfile

--- ci-cd/set_status.sh
+++ ci-cd/set_status.sh
@@ -15,5 +15,5 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2026-03-10" \
-  https://api.github.com/repos/$OWNER/$REPO/statuses/$env.GIT_COMMIT \
+  https://api.github.com/repos/$OWNER/$REPO/statuses/$GIT_COMMIT \
   -d "{\"state\":\"$STATE\",\"target_url\":\"$TARGET_URL\",\"description\":\"$DESC\",\"context\":\"$CONTEXT\"}"
