d3c622381b893b377e9936d487fbd22c49b24e4b
ogiusek <kowalewski.olgierd@gmail.com>
2026-05-09 21:13:17 +0200

    mend

--- engine/modules/graphics/internal/texture/compile.go
+++ engine/modules/graphics/internal/texture/compile.go
@@ -25,8 +25,8 @@ func newTexture(img image.Image) (uint32, error) {
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 	size := rgba.Rect.Size()
-	if size.X > math.MaxInt32 || size.Y > math.MaxInt32 {
-		return 0, fmt.Errorf("image dimensions exceed MaxInt32: %dx%d", size.X, size.Y)
+	if size.X < 0 || size.Y < 0 || size.X > math.MaxInt32 || size.Y > math.MaxInt32 {
+		return 0, fmt.Errorf("invalid image dimensions for OpenGL: %dx%d", size.X, size.Y)
 	}
 	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(size.X), int32(size.Y), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(rgba.Pix))
 
