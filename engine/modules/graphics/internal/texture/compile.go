1849f825817f222536d7a0bd688cdb592e934586
ogiusek <kowalewski.olgierd@gmail.com>
2026-05-09 21:11:07 +0200

    mend

--- engine/modules/graphics/internal/texture/compile.go
+++ engine/modules/graphics/internal/texture/compile.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/draw"
+	"math"
 
 	"github.com/go-gl/gl/v4.5-core/gl"
 )
@@ -23,8 +24,11 @@ func newTexture(img image.Image) (uint32, error) {
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(rgba.Rect.Size().X),
-		int32(rgba.Rect.Size().Y), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(rgba.Pix))
+	size := rgba.Rect.Size()
+	if size.X > math.MaxInt32 || size.Y > math.MaxInt32 {
+		return 0, fmt.Errorf("image dimensions exceed MaxInt32: %dx%d", size.X, size.Y)
+	}
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(size.X), int32(size.Y), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(rgba.Pix))
 
 	return texture, nil
 }
