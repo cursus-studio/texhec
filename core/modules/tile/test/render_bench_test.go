9c888f8944ba30b8b24ca14679b3f763021c47ae
ogiusek <kowalewski.olgierd@gmail.com>
2026-05-14 23:22:50 +0200

    created benchmark to prove rendering 1M tiles in <6ms on "UHD Graphics 620" integrated gpu performance claim

---
+++ core/modules/tile/test/render_bench_test.go
@@ -0,0 +1,75 @@
+package test
+
+import (
+	"core/modules/generation"
+	"engine/modules/camera"
+	"engine/modules/entityregistry"
+	"engine/modules/grid"
+	"engine/modules/loop"
+	"engine/modules/seed"
+	"engine/services/ecs"
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/jaypipes/ghw"
+	"github.com/ogiusek/events"
+)
+
+type Tiles struct {
+	Water    ecs.EntityID `path:"tiles/water.biom" tile:"" generate:"25"`
+	Sand     ecs.EntityID `path:"tiles/sand.biom" tile:"" generate:"25"`
+	Grass    ecs.EntityID `path:"tiles/grass.biom" tile:"" generate:"25"`
+	Mountain ecs.EntityID `path:"tiles/mountain.biom" tile:"" generate:"25"`
+}
+
+func BenchmarkRendering1MTilesMap(b *testing.B) {
+	if gpu, err := ghw.GPU(); err != nil {
+		b.Error(err)
+		return
+	} else {
+		for i, gpu := range gpu.GraphicsCards {
+			name := gpu.DeviceInfo.Product.Name
+			if i == 0 {
+				fmt.Printf("gpu: %v\n", name)
+			} else {
+				fmt.Printf("gpu %v: %v\n", i+1, name)
+			}
+		}
+	}
+
+	s := NewSetup()
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	events.GlobalErrHandler(s.EventsBuilder(), s.Logger().Log)
+
+	errs := ecs.RegisterSystems(
+		s.Render(),
+		s.Tile().Renderer(),
+	)
+	for _, err := range errs {
+		s.Logger().Log(err)
+	}
+
+	// before generating load elements
+	_, err := entityregistry.GetRegistry[Tiles](s.EntityRegistry())
+	s.Logger().Log(err)
+
+	gridEntity := s.World().NewEntity()
+
+	config := generation.NewConfig(gridEntity, seed.New(21377137), grid.NewCoords(1000, 1000))
+	s.Generation().Generate(config).Perform()
+
+	gameCamera := s.World().NewEntity()
+	ortho := camera.NewOrtho(-1000, +1000)
+	ortho.Zoom = 0.01
+	s.Camera().Ortho().Set(gameCamera, ortho)
+
+	events.Emit(s.Events(), loop.FrameEvent{})
+	b.ResetTimer()
+
+	for b.Loop() {
+		events.Emit(s.Events(), loop.FrameEvent{})
+	}
+}
--- core/modules/tile/test/setup.go
+++ core/modules/tile/test/setup.go
@@ -3,7 +3,10 @@ package test
 import (
 	"core/game"
 	corepkg "core/pkg"
+	assetspkg "engine/modules/assets/pkg"
+	"engine/modules/window"
 
+	"github.com/go-gl/gl/v4.5-core/gl"
 	"github.com/ogiusek/ioc/v2"
 )
 
@@ -12,7 +15,16 @@ type Setup struct {
 }
 
 func NewSetup() Setup {
-	c := ioc.NewContainer(corepkg.Pkg)
-	setup := ioc.GetServices[Setup](c)
-	return setup
+	c := ioc.NewContainer(
+		corepkg.Pkg,
+		func(b ioc.Builder) {
+			ioc.Wrap(b, func(c ioc.Dic, w window.Service) {
+				w.Window().SetTitle("tile module benchmark")
+				gl.ClearColor(0.2, 0.3, 0.3, 1.0)
+			})
+			ioc.Wrap(b, func(c ioc.Dic, b assetspkg.Config) { b.SetPath("../../../assets/") })
+		},
+	)
+	s := ioc.GetServices[Setup](c)
+	return s
 }
